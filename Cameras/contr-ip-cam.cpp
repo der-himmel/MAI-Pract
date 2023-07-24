308bbfece76d8696abf1b37154af45ea7f300a4e
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-07-24 21:37:59 +0300

    Update contr-ip-cam.cpp

--- Cameras/contr-ip-cam.cpp
+++ Cameras/contr-ip-cam.cpp
@@ -1,8 +1,9 @@
-//Управление зумом и фокусом камеры
+//zoom (+focus)
 
 #include <iostream>
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
+#include <time.h>
 
 using namespace web;
 using namespace web::http;
@@ -10,22 +11,42 @@ using namespace web::http::client;
 
 int main() {
     // IP address and port of the camera
-    std::string cameraIpAddress = "192.168.1.100";
+    std::string cameraIpAddress = "172.16.0.5";
     std::string cameraPort = "80";
-
-    // Command for Zoom (adjusting zoom level)
-    uri_builder zoomUri;
-    zoomUri.set_scheme("http")
+    
+    std::string username = "admin";
+    std::string password = "Admin123";
+    
+    uri_builder zoomOutUri;
+    zoomOutUri.set_scheme("http")
            .set_host(cameraIpAddress)
            .set_port(cameraPort)
-           .set_path("/zoom.cgi")
+           .set_path("/ptzctrl.cgi?-step=0&-act=zoomout&-speed=1")
+           .append_query("zoom", "out");
+    
+    uri_builder zoomInUri;
+    zoomInUri.set_scheme("http")
+           .set_host(cameraIpAddress)
+           .set_port(cameraPort)
+           .set_path("/ptzctrl.cgi?-step=0&-act=zoomin&-speed=1")
            .append_query("zoom", "in");
-    http_client client(zoomUri.to_uri());
-    client.request(methods::GET).then([](http_response response) {
-        std::cout << "Zoom command sent. Response code: " << response.status_code() << std::endl;
-    }).wait();
+           
+    http_client_config config;
+    config.set_credentials(web::credentials(username, password));
+    
+    http_client client(zoomOutUri.to_uri(), config); //or http_client client(zoomInUri.to_uri(), config)
+    
+    while(true)
+    {
+		client.request(methods::GET).then([](http_response response) {
+			std::cout << "Zoom out command sent. Response code: " << response.status_code() << std::endl;
+		}).wait();
+		sleep(1);
+	}
 
-    // Command for Focus (adjusting focus level)
+    
+
+    /*// Command for Focus (adjusting focus level)
     uri_builder focusUri;
     focusUri.set_scheme("http")
             .set_host(cameraIpAddress)
@@ -35,7 +56,8 @@ int main() {
     client = http_client(focusUri.to_uri());
     client.request(methods::GET).then([](http_response response) {
         std::cout << "Focus command sent. Response code: " << response.status_code() << std::endl;
-    }).wait();
+    }).wait();*/
 
     return 0;
 }
+
