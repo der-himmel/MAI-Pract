1e93025c1d2a5a09f02d982489bb94ca3c760950
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-07-26 15:16:16 +0300

    Update client.py

--- net/client.py
+++ net/client.py
@@ -1,30 +1,43 @@
 import socket
-import time
+import cv2
+import numpy as np
+import struct
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = '127.0.0.1'
+PORT = 11111
+client_socket.connect((HOST, PORT))
+
+data = b""
+payload_size = struct.calcsize("Q")
 
 while True:
-    try:
-        client.connect(('192.168.2.13', 2020))  # Connect to the server
-        print("Connected to the server.")
+    while len(data) < payload_size:
+        packet = client_socket.recv(4*1024)
+        if not packet: break
+        data += packet
 
-        file = open('image.jpg', 'rb')
-        image_data = file.read(2048)
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-        while image_data:
-            client.send(image_data)
-            image_data = file.read(2048)
+    while len(data) < msg_size:
+        data += client_socket.recv(4*1024)
 
-        file.close()
-        client.close()
-        print("Image sent successfully.")
-        break  # Exit the loop if the image transfer is successful
-    except ConnectionRefusedError:
-        print("Connection to the server refused. Retrying in 5 seconds...")
-        time.sleep(5)  # Wait for 5 seconds before retrying
-    except FileNotFoundError:
-        print("File 'image.jpg' not found. Please check the file path.")
-        break  # Exit the loop if the file is not found
-    except Exception as e:
-        print("An error occurred:", str(e))
-        break  # Exit the loop if any other error occurs
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    # Convert the received data back to a numpy array (frame)
+    nparr = np.frombuffer(frame_data, np.uint8)
+
+    # Decode the frame
+    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print("getting frames...")
+
+    cv2.imshow("OUTPUT", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+client_socket.close()
+cv2.destroyAllWindows()
