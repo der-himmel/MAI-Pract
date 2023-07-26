e595b2a72d8c5be75bbb9fd4b6ca09d5cfa112d6
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-07-26 15:52:42 +0300

    Update Contr.cpp

--- src/Contr.cpp
+++ src/Contr.cpp
@@ -1,17 +1,24 @@
-#include "opencv2/opencv.hpp"
-#include <iostream>
 #include <string>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <vector>
 #include "Contr.h"
 #include "RangeF.h"
 #include "Cam.h"
 
+using namespace std;
+using namespace boost::asio;
+
 Contr::Contr(Cam* cam, RangeF* range)
+	: cam(cam), range(range), acceptor(ioService, ip::tcp::endpoint(ip::tcp::v4(), 11111)),
+	clientSocket(ioService)
 {
-	//this->flag = 1;
-	//this->status = 0;
-	this->cam = cam;
-	this->range = range;
-};
+	acceptor.accept(clientSocket);
+	cout << "Established connection with: " << clientSocket.remote_endpoint() << endl;
+}
+
 
 void Contr::operating()
 {
@@ -28,10 +35,10 @@ void Contr::recording()
 
 	distance = std::to_string(range->dist);
 
-	cv::putText(cam->res, distance, cv::Point(30, 30), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255), 2);
+	cv::putText(cam->res, distance, cv::Point(100, 100), cv::FONT_HERSHEY_SIMPLEX, 3.0, cv::Scalar(255, 255, 255), 4);
 	//cv::imshow("RTSP stream + Thermal output", cam->res);
 
-	bool success = cv::imwrite(cam->filename, cam->res);
+	/*bool success = cv::imwrite(cam->filename, cam->res);
 
 	if (success)
 	{
@@ -40,8 +47,24 @@ void Contr::recording()
 	else
 	{
 		std::cout << "Error: Image not saved." << std::endl;
-	}
+	}*/
 
-	open = cv::imread(cam->filename);
-	cv::imshow("Image", open);
+	std::vector<uchar> encoded;
+	cv::imencode(".jpg", cam->res, encoded);
+
+	uint64_t msg_size = encoded.size();
+	clientSocket.write_some(boost::asio::buffer(&msg_size, sizeof(msg_size)));
+	clientSocket.write_some(boost::asio::buffer(encoded.data(), encoded.size()));
+
+	cv::imshow("SOURCE", cam->res);
+	char key = cv::waitKey(1);
+	if (key == 'q') {
+		clientSocket.close();
+		std::cout << "Connection closed" << std::endl;
+	}
+		
+	acceptor.close();
+
+	//open = cv::imread(cam->filename);
+	//cv::imshow("Image", open);
 }
