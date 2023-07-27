5743ed19e610c5f258c08f98cab1018d003b5c87
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-07-27 19:18:30 +0300

    Create server.py

---
+++ net/server.py
@@ -0,0 +1,41 @@
+import socket
+import cv2
+import numpy as np
+import struct
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = '127.0.0.1'
+PORT = 11111
+client_socket.connect((HOST, PORT))
+
+data = b""
+payload_size = struct.calcsize("Q")
+
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(4*1024)
+        if not packet: break
+        data += packet
+
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        data += client_socket.recv(4*1024)
+
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    nparr = np.frombuffer(frame_data, np.uint8)
+
+    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print("getting frames...")
+
+    cv2.imshow("OUTPUT", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+client_socket.close()
+cv2.destroyAllWindows()
