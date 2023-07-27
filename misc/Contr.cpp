9f759370187f3d77bf9f3fa04da4976478cc7467
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-07-27 17:48:56 +0300

    Update Contr.cpp

--- misc/Contr.cpp
+++ misc/Contr.cpp
@@ -7,15 +7,15 @@
 #include <thread>
 #include <future>
 #include <time.h>
-#include <cpprest/http_client.h>
-#include <cpprest/filestream.h>
+//#include <cpprest/http_client.h>
+//#include <cpprest/filestream.h>
 #include "Contr.h"
 #include "RangeF.h"
 #include "Cam.h"
 
-using namespace web;
-using namespace web::http;
-using namespace web::http::client;
+//using namespace web;
+//using namespace web::http;
+//using namespace web::http::client;
 using namespace std;
 using namespace boost::asio;
 
@@ -26,7 +26,7 @@ Contr::Contr(Cam* cam, RangeF* range)
 	acceptor.accept(clientSocket);
 	cout << "Established connection with: " << clientSocket.remote_endpoint() << endl;
 
-	uri_builder zoomOutUri;
+	/*uri_builder zoomOutUri;
 	zoomOutUri.set_scheme("http")
 		.set_host(cameraIpAddress)
 		.set_port(cameraPort)
@@ -60,7 +60,7 @@ Contr::Contr(Cam* cam, RangeF* range)
 	http_client zout(zoomOutUri.to_uri(), config);
 	http_client zin(zoomInUri.to_uri(), config);
 	http_client fout(focusOutUri.to_uri(), config);
-	http_client fin(focusInUri.to_uri(), config);
+	http_client fin(focusInUri.to_uri(), config);*/
 }
 
 
@@ -75,14 +75,16 @@ void Contr::init()
 void Contr::recording()
 {
 	cam->update();
+	//range->getPacket();
 
 	cv::putText(cam->res, std::to_string(range->dist), cv::Point(100, 100), cv::FONT_HERSHEY_SIMPLEX, 3.0, cv::Scalar(255, 255, 255), 4);
+	std::cout << range->dist << " meters." << std::endl << std::endl;
 	
 	cv::imshow("SOURCE", cam->res);
 	//cv::waitKey(1);
 
 	std::vector<uchar> encoded;
-	cv::imencode(".png", cam->res, encoded);
+	cv::imencode(".jpg", cam->res, encoded);
 
 	uint64_t msg_size = encoded.size();
 	clientSocket.write_some(boost::asio::buffer(&msg_size, sizeof(msg_size)));
@@ -94,21 +96,21 @@ void Contr::recording()
 		std::cout << "Connection closed" << std::endl;
 	}
 
-	//acceptor.close();
+	acceptor.close();
 }
 
 void Contr::listening()
 {
 	range->getPacket();
 	
-	std::vector<char> data(512);
+	/*std::vector<char> data(512);
 	size_t len = clientSocket.read_some(buffer(data));
 
 	if (len > 0) 
 	{
 		string received_data(data.begin(), data.begin() + len);
 		//cout << "Received: " << received_data << endl;
-		/*if (received_data == "zoomin")
+		if (received_data == "zoomin")
 		{
 			std::cout << "Zoom in command sent" << std::endl;
 
@@ -143,11 +145,11 @@ void Contr::listening()
 				{
 					std::cout << "Focus out command sent. Response code: " << response.status_code() << std::endl;
 				}).wait();
-		}*/
+		}
 	}
 
-	data.clear();
-	sleep(0.25);
+	data.clear();*/
+	sleep(0.5);
 }
 
 void Contr::operating()
@@ -155,9 +157,13 @@ void Contr::operating()
 	auto cams = std::async(&Contr::recording, this);
 	auto comm = std::async(&Contr::listening, this);
 	
+	//std::thread cams(&Contr::recording,this);
+	//std::thread comm(&Contr::listening,this);
+	
+	
+	//if(cams.joinable()){cams.join();}
+	//if(comm.joinable()){comm.join();}
+	
 	//if(cams.joinable()){cams.detach();}
 	//if(comm.joinable()){comm.detach();}
-	
-	//cams.join();
-	//comm.join();
 }
