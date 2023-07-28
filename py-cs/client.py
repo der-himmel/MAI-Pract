b619f3e6d60a34606bc840e598c996f5d806573f
der-himmel <105467009+der-himmel@users.noreply.github.com>
2023-07-28 16:43:59 +0300

    Update client.py

--- py-cs/client.py
+++ py-cs/client.py
@@ -7,69 +7,112 @@ import keyboard
 import time
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = "127.0.0.1"
-PORT = 11111
-client_socket.connect((HOST, PORT))
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# HOST = "127.0.0.1"
+# PORT = 11111
+# client_socket.connect((HOST, PORT))
+
+class cvStreamClient:
+    def __init__(self, HOST) -> None:
+        self.data = b""
+        self.payload_size = struct.calcsize("Q")
+        self.HOST = HOST
+        self.PORT_CV = 3333
+
+        self.clientCV = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientCV.connect((HOST, self.PORT_CV))
+    
+    def videoStream(self) -> None:
+        while True:
+            while len(self.data) < self.payload_size:
+                packet = self.clientCV.recv(4 * 1024)
+                if not packet:
+                    break
+                data += packet
+            packed_msg_size = self.data[:self.payload_size]
+            self.data = data[self.payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+            while len(data) < msg_size:
+                data += self.clientCV.recv(4 * 1024)
+
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+
+            nparr = np.frombuffer(frame_data, np.uint8)
+            frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            # print("getting frames...")
+
+            cv2.imshow("OUTPUT", frame)
+            cv2.waitKey(1)
+
+        self.clientCV.close()
+        cv2.destroyAllWindows()
 
 
-def video_stream():
-    data = b""
-    payload_size = struct.calcsize("Q")
+# def video_stream():
+#     data = b""
+#     payload_size = struct.calcsize("Q")
 
-    while True:
-        while len(data) < payload_size:
-            packet = client_socket.recv(4 * 1024)
-            if not packet:
-                break
-            data += packet
+#     while True:
+#         while len(data) < payload_size:
+#             packet = client_socket.recv(4 * 1024)
+#             if not packet:
+#                 break
+#             data += packet
 
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q", packed_msg_size)[0]
+#         packed_msg_size = data[:payload_size]
+#         data = data[payload_size:]
+#         msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-        while len(data) < msg_size:
-            data += client_socket.recv(4 * 1024)
+#         while len(data) < msg_size:
+#             data += client_socket.recv(4 * 1024)
 
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
+#         frame_data = data[:msg_size]
+#         data = data[msg_size:]
 
-        nparr = np.frombuffer(frame_data, np.uint8)
-        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        # print("getting frames...")
+#         nparr = np.frombuffer(frame_data, np.uint8)
+#         frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#         # print("getting frames...")
 
-        cv2.imshow("OUTPUT", frame)
-        cv2.waitKey(1)
+#         cv2.imshow("OUTPUT", frame)
+#         cv2.waitKey(1)
         
-        #key = cv2.waitKey(2) & 0xFF
-        #if key == ord("q"):
-        #    break
+#         #key = cv2.waitKey(2) & 0xFF
+#         #if key == ord("q"):
+#         #    break
 
-    client_socket.close()
-    cv2.destroyAllWindows()
+#     client_socket.close()
+#     cv2.destroyAllWindows()
 
 
-def comm_thread():
-    comm = ""
+class camSend:
+    def __init__(self, HOST) -> None:
+        self.comm = ""
+        self.HOST = HOST
+        self.PORT_C = 1111
 
-    while True:
-        if keyboard.is_pressed("up"):
-            comm = "zoomin"
-            print(comm)
-        elif keyboard.is_pressed("down"):
-            comm = "zoomout"
-            print(comm)
-        elif keyboard.is_pressed("left"):
-            comm = "focusin"
-            print(comm)
-        elif keyboard.is_pressed("right"):
-            comm = "focusout"
-            print(comm)
-        else:
-            comm = ""
+        self.clientCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientCS.connect((HOST, self.PORT_C))
 
-        client_socket.sendall(comm.encode("utf-8"))
-        time.sleep(0.5)
+    def sendComm(self) -> None:
+
+        while True:
+            if keyboard.is_pressed("up"):
+                self.comm = "zoomin"
+            elif keyboard.is_pressed("down"):
+                self.comm = "zoomout"
+            elif keyboard.is_pressed("left"):
+                self.comm = "focusout"
+            elif keyboard.is_pressed("right"):
+                self.comm = "focusin"
+            else:
+                self.comm = ""
+
+            if self.comm:
+               print(self.comm)
+            self.clientCS.sendall(self.comm.encode("utf-8"))
+            time.sleep(0.5)
 
     # while True:
     #     data = conn.recv(1024).decode("utf-8")
@@ -91,8 +134,14 @@ def comm_thread():
 
 
 if __name__ == "__main__":
-    video_thread = threading.Thread(target=video_stream)
-    comm_thread = threading.Thread(target=comm_thread)
+    HOST = "127.0.0.1"
+
+    cams_stream = cvStreamClient(HOST)
+    ipcam_comm = camSend(HOST)
+    # distance_rec = 
+
+    video_thread = threading.Thread(target=cams_stream.videoStream)
+    comm_thread = threading.Thread(target=ipcam_comm.sendComm)
 
     video_thread.start()
     comm_thread.start()
